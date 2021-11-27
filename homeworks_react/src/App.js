eb7bf9946c7588ea5079b7c4b2b5f72f71a3f13d
Nikitoskaluv <anastasiyak90@mail.ru>
2021-11-27 16:03:58 +0300

    Homework_1

--- homeworks_react/src/App.js
+++ homeworks_react/src/App.js
@@ -1,2 +1,16 @@
 
 import './App.css';
+const stylesForWrapper = {
+  padding: "20px",
+  textAlign: "center",
+}
+
+function App(props) {
+  return (
+    <div className="messageWrapper" style={stylesForWrapper}>
+      <h1 className="title">Переданнный текст: <span style={{ fontStyle: "italic", color: "black" }}>{props.message}</span></h1>
+    </div>
+  );
+}
+
+export default App;
--- homeworks_react/src/index.js
+++ homeworks_react/src/index.js
@@ -1,9 +1,17 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const messageText = "Now that we know how to render stuff, let's make our app a little more complex by introducing child elements. ";
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App message={messageText} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 reportWebVitals();
